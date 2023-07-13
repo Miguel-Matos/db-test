d9adc95ab933ae2c52203f13c07af9b50225eccc
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-07-13 16:28:26 +0900

    added db

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+  databaseURL: 'https://db-test-bdcbe-default-rtdb.firebaseio.com/'
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const items = ref(database, 'groceries');
+
 const inputVal = document.getElementById('input-field');
 
 const btn = document.getElementById('add-button');
 
+const list = document.getElementById("shopping-list");
+const li = document.createElement('li');
+
+
 btn.addEventListener('click', () => {
-  console.log(inputVal.value)
+  li.textContent = inputVal.value;
+  list.appendChild(li);
+  push(items, inputVal.value);
+
+  inputVal.value = '';
 })
