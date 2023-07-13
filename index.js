b58dc85632c5db232de7fdc47186b06a1a63d62c
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-07-13 15:51:16 +0900

    created test files

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputVal = document.getElementById('input-field');
+
+const btn = document.getElementById('add-button');
+
+btn.addEventListener('click', () => {
+  console.log(inputVal.value)
+})
